ce86d15a20c920e272ac02ca6f23b2e0dfad17a0
farjamazizi <farjamazizi@gmail.com>
2021-06-09 15:16:51 +0430

    adding config files to project

---
+++ tiger/__init__.py
@@ -0,0 +1,42 @@
+from os.path import dirname
+
+from nanohttp import settings
+from restfulpy import Application
+
+from .controllers.root import Root
+from . import basedata
+from . import mockup
+
+
+__version__ = '0.1'
+
+
+class Tiger(Application):
+    __configuration__ = '''
+      db:
+        url: postgresql://postgres:postgres@localhost/tiger
+        test_url: postgresql://postgres:postgres@localhost/tiger_test
+        administrative_url: postgresql://postgres:postgres@localhost/postgres
+   '''
+
+    def __init__(self, application_name='tiger', root=Root()):
+        super().__init__(
+            application_name,
+            root=root,
+            root_path=dirname(__file__),
+            version=__version__,
+        )
+
+    def insert_basedata(self, *args):
+        basedata.insert()
+
+    def insert_mockup(self, *args):
+        mockup.insert()
+
+    @classmethod
+    def initialize_orm(cls, engine=None):
+        super().initialize_orm(cls, engine)
+
+
+tiger = Tiger()
+
