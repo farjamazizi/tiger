aeb4dd88ba8bdff8dc6008171408fa0397dcece1
farjamazizi <farjamazizi@gmail.com>
2021-06-12 16:50:43 +0430

    I had a make some changes in setup.py

--- setup.py
+++ setup.py
@@ -3,15 +3,11 @@ from setuptools import setup, find_packages
 
 dependencies = [
     'gunicorn',
-    'attrs == 20.3.0',
     'iniconfig == 1.1.1',
-    'itsdangerous == 1.1.0',
     'packaging == 20.9',
-    'pluggy == 0.13.1',
     'py == 1.10.0',
     'pyparsing == 2.4.7',
     'pytest == 6.2.2',
-    'toml == 0.10.2',
     'bddrest == 2.6.2',
     'nanohttp == 1.11.11',
     'restfulpy == 3.8.0',
