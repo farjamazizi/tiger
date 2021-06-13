234c7fc7c3825e3762ae49012284e8f4ad637f65
farjamazizi <farjamazizi@gmail.com>
2021-06-13 16:24:43 +0430

    I had a make some changes in init and models file

--- tiger/models/message.py
+++ tiger/models/message.py
@@ -1,16 +1,15 @@
 from sqlalchemy import Column, Integer, ForeignKey, Text
 from sqlalchemy.orm import relationship
-from restfulpy.orm import DeclarativeBase, Field, OrderingMixin, \
-    FilteringMixin, PaginationMixin
+from restfulpy.orm import DeclarativeBase, Field
 
 
 class Message(DeclarativeBase):
     __tablename__ = 'message'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    text = Column(Text)
-    sender_id = Column(Integer, ForeignKey('member.id'))
-    room_id = Column(Integer, ForeignKey('room.id'))
+    id = Field(Integer, primary_key=True, autoincrement=True)
+    text = Field(Text)
+    sender_id = Field(Integer, ForeignKey('member.id'))
+    room_id = Field(Integer, ForeignKey('room.id'))
 
     room = relationship(
         'Room',
