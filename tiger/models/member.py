1559ad3e7230778524dc28e233d5f9b6e2bc654b
farjamazizi <46857447+farjamazizi@users.noreply.github.com>
2021-06-14 09:04:42 +0430

    Adding member model to project in model directory (#6)
    * Adding member model to project
    
    * adding that model to init.py
    
    * code style
    
    * I had a make some changes in init and models file
    
    * I had a make some changes in membermodel

---
+++ tiger/models/member.py
@@ -0,0 +1,19 @@
+from datetime import date
+
+from sqlalchemy import String, Date, Integer, extract
+from sqlalchemy.orm import column_property
+from restfulpy.orm import DeclarativeBase, Field
+
+
+class Member(DeclarativeBase):
+    __tablename__ = 'member'
+
+    id = Field(Integer, primary_key=True, autoincrement=True)
+    first_name = Field(String)
+    last_name = Field(String)
+    user_name = Field(String, unique=True)
+    password = Field(String)
+    birth_date = Field(Date)
+    age = column_property(date.today().year - extract('year', birth_date))
+    fullname = column_property(first_name + ' ' + last_name)
+
