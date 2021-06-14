544c29d4745c953d94779a9f341f7f3709a5668b
farjamazizi <farjamazizi@gmail.com>
2021-06-14 12:41:34 +0430

    adding roommodel to project in model directory

--- tiger/models/__init__.py
+++ tiger/models/__init__.py
@@ -1,3 +1,4 @@
 from .member import Member
 from .message import Message
+from .room import Room, RoomAdmin, RoomMember
 
---
+++ tiger/models/room.py
@@ -0,0 +1,39 @@
+from sqlalchemy import  Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from restfulpy.orm import DeclarativeBase, Field
+
+class RoomMember(DeclarativeBase):
+    __tablename__ = 'room_member'
+
+    member_id= Field(Integer, ForeignKey('member.id'), primary_key=True)
+    room_id= Field(Integer, ForeignKey('room.id'), primary_key=True)
+
+
+class RoomAdmin(DeclarativeBase):
+    __tablename__ = 'room_admin'
+
+    admin_id= Field(Integer, ForeignKey('member.id'), primary_key=True)
+    room_id= Field(Integer, ForeignKey('room.id'), primary_key=True)
+
+
+class Room(DeclarativeBase):
+    __tablename__ = 'room'
+
+    id = Field(Integer, primary_key=True)
+    title = Field(String)
+
+    messages = relationship(
+        'Message',
+        back_populates='room',
+    )
+    members=relationship(
+        'Member',
+        secondary='room_member',
+        back_populates='rooms',
+    )
+
+    admins = relationship(
+        'Member',
+        secondary='room_admin',
+        back_populates='admin_rooms',
+    )
