dea7343ace03abfc70900e46857794836d88db2e
farjamazizi <farjamazizi@gmail.com>
2021-06-28 10:35:01 +0430

    adding error report model

---
+++ tiger/models/error_report.py
@@ -0,0 +1,17 @@
+from sqlalchemy import String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from restfulpy.orm import DeclarativeBase, Field
+
+
+class Error_Report(DeclarativeBase):
+    __tablename__ = 'error_report'
+
+    curl = Field(String)
+    stack_trace = Field(String)
+    status = Field(String)
+    organization = Field(String)
+    created_at = Field(String)
+    modified_at = Field(String)
+    member_id = Field(Integer, ForeignKey('member.id'))
+
+
