ea9d01abeffaa1af9163dc2ab8bf48a9a523e553
farjamazizi <farjamazizi@gmail.com>
2021-06-28 11:06:14 +0430

    adding error report model

--- tiger/models/error_report.py
+++ tiger/models/error_report.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 from sqlalchemy import String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
 from restfulpy.orm import DeclarativeBase, Field
@@ -10,9 +12,13 @@ class Error_Report(DeclarativeBase):
     curl = Field(String)
     stack_trace = Field(String)
     status = Field(String)
-    organization = Field(Integer)
-    created_at = Field(String)
-    modified_at = Field(String)
+    organization_id = Field(Integer)
+    created_at = Field(date)
+    modified_at = Field(date)
     member_id = Field(Integer, ForeignKey('member.id'))
-    member = relationship('Member', back_populates='error_reports')
+   
+    member = relationship(
+        'Member',
+        back_populates='error_reports',
+    )
 
--- tiger/models/member.py
+++ tiger/models/member.py
@@ -24,10 +24,13 @@ class Member(DeclarativeBase):
         Enum(*member_statuses, name='member_statuses'),
         nullable=False,
     )
-    error_reports = relationship('Error_Report', back_populates='member')
     age = column_property(date.today().year - extract('year', birth_date))
     fullname = column_property(first_name + ' ' + last_name)
 
+    error_reports = relationship(
+        'Error_Report',
+        back_populates='member',
+    )
     messages = relationship(
         'Message',
         back_populates='sender',
